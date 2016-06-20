f0aa0306393086172b0f52c0f7da54e591a819df
Frank Wegener <wegener-it@cc-email.eu>
2016-06-20 15:25:33 +0200

    foto-install.py improved, cann handle .mov files as well

--- foto-install.py
+++ foto-install.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # this script require:
-#   apt-get install exiv2
+#   apt-get install libimage-exiftool-perl
 
 
 import os
@@ -10,8 +10,9 @@ import subprocess
 import sys
 
 PATH_OFFEN = "/mnt/_k/__offen"
-PATH_JPG   = "/mnt/_k/owncloud/fwe/_fwe-photo-offen-jpg"
-PATH_RAW   = "/mnt/_k/owncloud/fwe/_fwe-photo-offen-raw"
+PATH_JPG   = "/mnt/_k/owncloud/_fwe-photo-offen-jpg"
+PATH_RAW   = "/mnt/_k/owncloud/_fwe-photo-offen-raw"
+PATH_MOV   = "/mnt/_k/owncloud/_fwe-video-offen"
 
 
 def error(msg):
@@ -23,22 +24,42 @@ def is_jpgfile(f):
     return (len(f) > 4 and f[-4:].lower() == ".jpg")
 
 
+def is_movfile(f):
+    return (len(f) > 4 and f[-4:].lower() == ".mov")
+
+
 def jpgfile_date(jpg_file):
-    output = subprocess.check_output(['exiv2', '-q', jpg_file])
+    output = subprocess.check_output(['exiftool', '-q', jpg_file])
 
     for line in output.split('\n'):
-        if "Image timestamp" in line:
+        if "Create Date" in line:
             # Image timestamp : 2014:10:04 17:17:58
-            # Zeitstempel des Bildes: 2014:10:04 17:17:58
-            year   = line[18:22]
-            month  = line[23:25]
-            day    = line[26:28]
+            year   = line[34:38]
+            month  = line[39:41]
+            day    = line[42:44]
             return "%s-%s-%s" % (year, month, day)
- 
+
     error("No timestamp information in file '%s' " % (raw_file))
 
 
-def rename(jpgfile, date, nr):
+def movfile_timestamp(jpg_file):
+    output = subprocess.check_output(['exiftool', '-q', jpg_file])
+
+    for line in output.split('\n'):
+        if "Media Create Date" in line:
+            # Zeitstempel des Bildes: 2014:10:04 17:17:58
+            year   = line[34:38]
+            month  = line[39:41]
+            day    = line[42:44]
+            hour   = line[45:47]
+            minut  = line[48:50]
+            secs   = line[51:53]
+            return ("%s-%s-%s" % (year, month, day), "%s:%s.%s" % (hour, minut, secs))
+
+    error("No timestamp information in file '%s' " % (raw_file))
+
+
+def rename_photo(jpgfile, date, nr):
     path_raw    = PATH_RAW + '/' + date
     path_jpg    = PATH_JPG + '/' + date
     new_rawfile = "%s/%s-%04d.orf" % (path_raw, date, nr)
@@ -58,6 +79,16 @@ def rename(jpgfile, date, nr):
         shutil.move(rawfile, new_rawfile)
 
 
+def rename_video(movfile, date, timestamp, nr):
+    path_mov    = PATH_MOV + '/' + date
+    new_movfile = "%s/%s__%s__%04d.mov" % (path_mov, date, timestamp, nr)
+
+    if not os.path.exists(path_mov):
+        os.mkdir(path_mov, 0750)
+
+    shutil.move(movfile, new_movfile)
+
+
 def main():
     date = "xxxx"
     nr   = 0
@@ -65,11 +96,8 @@ def main():
     for f in sorted(os.listdir(PATH_OFFEN)):
         f = PATH_OFFEN + "/" + f
 
-        if not os.path.isfile(f):
+        if not os.path.isfile(f) or not is_jpgfile(f):
             continue
-
-        if not is_jpgfile(f):
-           continue
 
         new_date = jpgfile_date(f)
  
@@ -77,8 +105,27 @@ def main():
             date = new_date
             nr   = 0
        
-        rename(f, date, nr)
-        nr  += 1
+        rename_photo(f, date, nr)
+        nr += 1
+        continue
 
+    date = "xxxx"
+    nr   = 0
+
+    for f in sorted(os.listdir(PATH_OFFEN)):
+        f = PATH_OFFEN + "/" + f
+
+        if not os.path.isfile(f) or not is_movfile(f):
+            continue
+
+        new_date, timestamp = movfile_timestamp(f)
+ 
+        if (new_date != date):
+            date = new_date
+            nr   = 0
+       
+        rename_video(f, date, timestamp, nr)
+        nr += 1
+        continue
 
 main()
